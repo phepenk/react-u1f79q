a3e3e68b8b080a18fdaabcc2bb38f5a0e17bd8ea
phepenk <phepenk@gmail.com>
2021-06-08 22:41:41 +1000

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,52 +1,90 @@
-import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import 'mapbox-gl/dist/mapbox-gl.css';
+import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css';
+import React, { useState, useRef, useCallback } from 'react';
+import MapGL from 'react-map-gl';
+import Geocoder from 'react-map-gl-geocoder';
 
-mapboxgl.accessToken =
+const MAPBOX_TOKEN =
   'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
 
 export default function App() {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [homeLng, setHomeLng] = useState(-70.9);
-  const [homeLat, setHomeLat] = useState(42.35);
-  const [salesmanLng, setSalesmanLng] = useState(-70.9);
-  const [salesmanLat, setSalesmanLat] = useState(42.35);
+  const [homeCoord, setHomeCoord] = useState({
+    lat: 42.35,
+    lng: -70.9
+  });
+  const [salesmanCoord, setSalesmanCoord] = useState({
+    lat: -70.9,
+    lng: 42.35
+  });
+
   const [zoom, setZoom] = useState(9);
 
-  useEffect(() => {
-    if (map.current) return; // wait for map to initialize
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [homeLng, homeLat],
-      zoom: zoom
-    });
-  });
+  // useEffect(() => {
+  //   if (map.current) return; // wait for map to initialize
+  //   map.current = new mapboxgl.Map({
+  //     container: mapContainer.current,
+  //     style: 'mapbox://styles/mapbox/streets-v11',
+  //     center: [homeLng, homeLat],
+  //     zoom: zoom
+  //   });
+  // });
 
-  useEffect(() => {
-    if (!map.current) return; // wait for map to initialize
-    map.current.on('move', () => {
-      setSalesmanLng(map.current.getCenter().lng.toFixed(4));
-      setSalesmanLat(map.current.getCenter().lat.toFixed(4));
-      setZoom(map.current.getZoom().toFixed(2));
-    });
-  });
+  // useEffect(() => {
+  //   if (!map.current) return; // wait for map to initialize
+  //   map.current.on('move', () => {
+  //     setSalesmanLng(map.current.getCenter().lng.toFixed(4));
+  //     setSalesmanLat(map.current.getCenter().lat.toFixed(4));
+  //     setZoom(map.current.getZoom().toFixed(2));
+  //   });
+  // });
 
-  useEffect(() => {
-    if (!map.current) return;
-    // Create a new marker
-    const salesmanLocation = [salesmanLng, salesmanLat];
-    const salesmanMarker = new mapboxgl.Marker()
-      .setLngLat(salesmanLocation)
-      .addTo(map);
+  // useEffect(() => {
+  //   if (!map.current) return;
+  //   // Create a new marker
+  //   const salesmanLocation = [salesmanLng, salesmanLat];
+  //   const salesmanMarker = new mapboxgl.Marker()
+  //     .setLngLat(salesmanLocation)
+  //     .addTo(map);
+  // });
+
+  const [viewport, setViewport] = useState({
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8
   });
+  const mapRef = useRef();
+  const handleViewportChange = useCallback(
+    newViewport => setViewport(newViewport),
+    []
+  );
+
+  // if you are happy with Geocoder default settings, you can just use handleViewportChange directly
+  const handleGeocoderViewportChange = useCallback(newViewport => {
+    const geocoderDefaultOverrides = { transitionDuration: 1000 };
+
+    return handleViewportChange({
+      ...newViewport,
+      ...geocoderDefaultOverrides
+    });
+  }, []);
 
   return (
-    <div>
-      <div className="sidebar">
-        Longitude: {salesmanLng} | Latitude: {salesmanLat} | Zoom: {zoom}
-      </div>
-      <div ref={mapContainer} className="map-container" />
+    <div style={{ height: '100vh' }}>
+      <MapGL
+        ref={mapRef}
+        {...viewport}
+        width="100%"
+        height="100%"
+        onViewportChange={handleViewportChange}
+        mapboxApiAccessToken={MAPBOX_TOKEN}
+      >
+        <Geocoder
+          mapRef={mapRef}
+          onViewportChange={handleGeocoderViewportChange}
+          mapboxApiAccessToken={MAPBOX_TOKEN}
+          position="top-left"
+        />
+      </MapGL>
     </div>
   );
 }
